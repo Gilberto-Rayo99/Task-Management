24e40f550a67ccbdd55cbb961e8744a220fdbb8e
Gilberto Rayo <123099535+Gilberto-Rayo99@users.noreply.github.com>
2023-10-18 13:39:36 -0600

    Delete client directory

--- client/src/api/axios.js
+++
@@ -1,7 +0,0 @@
-import axios from "axios";
-
-const instance = axios.create({
-  baseURL: "https://backendservicetask-management.onrender.com/api",
-  withCredentials: true,
-});
-export default instance;
