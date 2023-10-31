8adac782f425d3335adcd7a5a9b34f99887fd228
Gilberto-rayo99 <jg.rayo999@gmail.com>
2023-10-31 13:30:39 -0600

    update mongoose

--- src/db.js
+++ src/db.js
@@ -1,11 +1,13 @@
 import mongoose from "mongoose";
 
-export const connectDB=async()=>{
-    try {
-        await mongoose.connect("mongodb+srv://jgrayo999:oLfjcAoKgKZjP9Ou@cluster0.mbbasoq.mongodb.net/");
+export const connectDB = async () => {
+  try {
+    await mongoose.connect(
+      "mongodb+srv://jgrayo999:oLfjcAoKgKZjP9Ou@cluster0.mbbasoq.mongodb.net/?retryWrites=true&w=majority"
+    );
 
-        console.log("DB is connected ==>");
-    } catch (error) {
-        console.log(error);
-    }
+    console.log("DB is connected ==>");
+  } catch (error) {
+    console.log(error);
+  }
 };
