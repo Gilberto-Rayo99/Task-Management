f61f8b7ae5b3e898aec487b3d59d8c0799e2c86f
Gilberto Rayo <123099535+Gilberto-Rayo99@users.noreply.github.com>
2023-11-03 10:37:20 -0600

    Update app.js

--- src/app.js
+++ src/app.js
@@ -8,7 +8,7 @@ import taskRoutes from "./routes/tasks.routes.js";
 const app = express();
 app.use(
   cors({
-    origin: "task-management-production-81b6.up.railway.app",
+    origin: "https://frontendservicetask-management.onrender.com",
     credentials: true,
   })
 );
