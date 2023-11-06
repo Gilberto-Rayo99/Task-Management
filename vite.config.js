763dd93f900df4a08aa3c398aa3eb9400fdb5d5d
Gilberto Rayo <123099535+Gilberto-Rayo99@users.noreply.github.com>
2023-11-06 12:08:39 -0600

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
+// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/', // This should be the public path from which your app will be served.
   plugins: [react()],
-  base:'/<Task-Management>/'
-})
+  build: {
+    // other build options...
+    outDir: 'dist', // The output directory for build files
+    rollupOptions: {
+      // additional options if necessary
+    },
+  },
+  // If you need to set server-specific configurations:
+  server: {
+    // server options...
+  }
+});
